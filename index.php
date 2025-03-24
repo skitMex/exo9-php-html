d421f6f9e6b8fb3fd41fd5cb46d656f5785eb048
Martin Hurvois <martin@hurvois.fr>
2025-03-24 18:56:27 +0100

    first commit

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+require 'Views/pdo.php';
+?>
+<!DOCTYPE html>
+<?php
+$qeury = $pdo->query('SELECT * FROM etudiants');
+$etudiant = $qeury->fetchAll();
---
+++ pdo.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost"; // Nom du serveur
+$username = "root"; // Nom d'utilisateur de la base de données
+$password = "root"; // Mot de passe de la base de données
+$dbname = "nom_de_la_base_de_donnees"; // Nom de la base de données
+
+try {
+    $dbPDO = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Configuration de PDO pour générer des exceptions en cas d'erreur
+    $dbPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie à la base de données";
+} catch (PDOException $e) {
+    echo "La connexion a échouée : " . $e->getMessage();
+}
