2f5fcb9dfb7fa4bdbc4b06494b86363d008c82a8
Martin Hurvois <martin@hurvois.fr>
2025-03-26 19:18:18 +0100

    commit de base

--- Models/pdo.php
+++ Models/pdo.php
@@ -3,7 +3,7 @@
 $servername = "localhost"; // Nom du serveur
 $username = "root"; // Nom d'utilisateur de la base de données
 $password = "root"; // Mot de passe de la base de données
-$dbname = "nom_de_la_base_de_donnees"; // Nom de la base de données
+$dbname = "mise en place"; // Nom de la base de données
 
 try {
     $dbPDO = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'Views/pdo.php';
-?>
-<!DOCTYPE html>
-<?php
-$qeury = $pdo->query('SELECT * FROM etudiants');
-$etudiant = $qeury->fetchAll();
